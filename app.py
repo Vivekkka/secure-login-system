c5aaa17cd9286fb908199d4c0da348800a66603a
Vivek Kumar Digar <luck78088@gmail.com>
2026-01-20 14:11:34 +0530

    Day 3: Backend setup with Flask and SQLite database

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# User table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # Admin/User
+
 @app.route("/")
 def home():
-    return "Secure Login System Running"
+    return "Backend & Database Connected Successfully"
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
